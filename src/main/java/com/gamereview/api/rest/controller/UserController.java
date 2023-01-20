c9392f0ce1ee82e4e6c81105b30b582537b10aac
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2023-01-20 20:35:01 -0300

    First entity crud started

---
+++ src/main/java/com/gamereview/api/rest/controller/UserController.java
@@ -0,0 +1,50 @@
+package com.gamereview.api.rest.controller;
+
+import com.gamereview.api.entities.User;
+import com.gamereview.api.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> findAll(){
+        return userService.findAllUser();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findUserById(id);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        user = userService.createUser(user);
+        return ResponseEntity.ok().body(user);
+    }
+
+
+}
---
+++ src/main/java/com/gamereview/api/services/UserService.java
@@ -0,0 +1,44 @@
+package com.gamereview.api.services;
+
+import com.gamereview.api.entities.User;
+import com.gamereview.api.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user){
+      return this.userRepository.save(user);
+    }
+
+    public List<User> findAllUser(){
+      return this.userRepository.findAll();
+    }
+
+    public void deleteUser(Long id){
+      this.userRepository.deleteById(id);
+    }
+
+    public User findUserById(Long id){
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    public User updateUser(Long id, User user){
+        User obj = findUserById(id);
+        obj.setName(user.getName());
+        obj.setEmail(user.getEmail());
+        obj.setPassword(user.getPassword());
+        return this.userRepository.save(user);
+    }
+
+}
